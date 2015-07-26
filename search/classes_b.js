ac59b41963cfbb1c65c1e8e16f834717d1fea172
Titouan COLADON <titouan.coladon@gmail.com>
2015-07-26 15:04:00 +0200

    Update doc and tarball with bug GB=(1) solved with index.html

--- search/classes_b.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['taggedpolynomial',['TaggedPolynomial',['../class_f4_1_1_tagged_polynomial.html',1,'F4']]],
-  ['term',['Term',['../class_f4_1_1_term.html',1,'F4']]]
-];
--- search/variables_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['weight',['WEIGHT',['../class_f4_1_1_monomial.html#ad92cd2528e6d5deadbe5f0e58b183205',1,'F4::Monomial']]]
-];
